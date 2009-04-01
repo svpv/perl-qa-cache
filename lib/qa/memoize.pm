67ce87462f5b0dd76f5dcce2356da19bb0f2afc8
Alexey Tourbin <at@altlinux.ru>
2009-04-01 17:24:45 +0400

    qa/memoize.pm: implemented (basename,size,mtime) mode

--- lib/qa/memoize.pm
+++ lib/qa/memoize.pm
@@ -3,12 +3,28 @@ package qa::memoize;
 use strict;
 our $NOCACHE ||= $ENV{QA_NOCACHE};
 
-use qa::cache;
-use File::stat qw(stat);
+use constant {
+	st_ino => 1,
+	st_size => 7,
+	st_mtime => 9,
+};
 
-sub memoize_st1 ($) {
+use constant {
+	ISM => 0,
+	BSM => 1,
+};
+
+sub basename ($) {
+	$_[0] =~ m#(?>.*/)(.*[^.].*)#s or
+	$_[0] =~ m#(?:.*/)?([^/]*[^/.][^/]*)/*\z#s or
+		die "$_[0]: no valid basename";
+	return $1;
+}
+
+sub memoize_st1_ ($$) {
 	return if $NOCACHE;
 	my $id  = shift;
+	my $how = shift;
 	my $pkg = caller;
 	my $sym = $pkg . '::' . $id;
 	no strict 'refs';
@@ -17,23 +33,44 @@ sub memoize_st1 ($) {
 	no warnings 'redefine';
 	*$sym = sub ($) {
 		goto $code if $NOCACHE;
+		$cache ||= do {
+			require qa::cache;
+			qa::cache->TIEHASH($id);
+		};
 		my $f = shift;
-		$cache ||= qa::cache->TIEHASH($id);
-		my $st0 = stat($f) or die "$id: $f: $!";
-		my @ism0 = ($st0->ino, $st0->size, $st0->mtime);
-		my $v = $cache->FETCH("@ism0");
+		my @st0 = stat($f) or die "$id: $f: $!";
+		my $ism0 = join " " => @st0[st_ino,st_size,st_mtime];
+		my $k = ($how == ISM) ? $ism0 :
+			join " " => basename($f), @st0[st_size,st_mtime];
+		my $v = $cache->FETCH($k);
 		return $v if defined $v;
 		$v = $code->($f);
-		my $st1 = stat($f) or die "$id: $f: $!";
-		my @ism1 = ($st1->ino, $st1->size, $st1->mtime);
-		die "$id: $f: file has changed" unless "@ism0" eq "@ism1";
-		$cache->STORE("@ism0", $v) if defined $v;
+		my @st1 = stat($f) or die "$id: $f: $!";
+		my $ism1 = join " " => @st1[st_ino,st_size,st_mtime];
+		die "$id: $f: file has changed" unless $ism0 eq $ism1;
+		$cache->STORE($k, $v) if defined $v;
 		return $v;
 	}
 }
 
+# memoize a function which takes single file argument by (ino,size,mtime)
+sub memoize_ism ($) {
+	push @_, ISM;
+	goto &memoize_st1_;
+}
+
+# memoize a function which takes single file argument by (basename,size,mtime)
+sub memoize_bsm ($) {
+	push @_, BSM;
+	goto &memoize_st1_;
+}
+
+# compat
+*memoize_st1 = \&memoize_ism;
+
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(memoize_st1);
+our @EXPORT = qw(memoize_ism memoize_bsm);
+our @EXPORT_OK = qw(memoize_st1 basename);
 
 1;
